ce9f9be770b53af9bcb622b84faeb932247f4f41
Sudo114 <98070105+Sudo114@users.noreply.github.com>
2022-01-23 10:42:24 +0530

    Create Operators.c

---
+++ Operators.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+
+    int a;
+    int b ;
+    
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+    
+     printf("Enter the value of b\n");
+    scanf("%d",&b);
+
+    printf("The Value Of a+b is %d\n", a+b);
+    printf("The Value Of a-b is %d\n", a-b);
+    printf("The Value Of a*b is %d\n", a*b);
+    printf("The Value Of a%b is %d\n", a%b);
+    printf("The Value Of a/b is %d\n", a/b);
+
+
+    return 0;
+}
